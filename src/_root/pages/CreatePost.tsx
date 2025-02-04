3d3612560c607e94efb424f36fb9f07c3b584fb3
Djonahuti <djonah04@gmail.com>
2025-02-05 00:42:22 +0100

    commit

--- src/_root/pages/CreatePost.tsx
+++ src/_root/pages/CreatePost.tsx
@@ -1,4 +1,4 @@
-
+import PostForm from "@/components/forms/PostForm"
 
 const CreatePost = () => {
   return (
@@ -8,6 +8,7 @@ const CreatePost = () => {
           <img src="/assets/icons/add-post.svg" alt="add" width={36} height={36} />
           <h2 className="h3-bold md:h2-bold text-left w-full">Create Post</h2>
         </div>
+        <PostForm />
       </div>
     </div>
   )
