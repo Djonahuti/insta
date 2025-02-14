62e86c707b6ea4228de88083453b747af6c5d5c3
Djonahuti <djonah04@gmail.com>
2025-02-14 09:18:26 +0100

    commit

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -14,7 +14,7 @@ const AuthLayout = () => {
         </section>
 
         <img
-         src="/assets/images/side.png"
+         src="/assets/images/my-side.svg"
          alt="logo"
          className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
         />
